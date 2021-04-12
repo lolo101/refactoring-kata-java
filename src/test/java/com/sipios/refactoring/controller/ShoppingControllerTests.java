9dd3d161fa06aa9f5fbd16e33f96948fabf2dbe0
lolo101 <lbroquet@online.fr>
2021-04-12 12:37:24 +0200

    Add controller test about discount

--- src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
+++ src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
@@ -3,6 +3,8 @@ package com.sipios.refactoring.controller;
 import com.sipios.refactoring.UnitTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 
 import static com.sipios.refactoring.controller.ClientType.STANDARD_CUSTOMER;
@@ -24,5 +26,11 @@ class ShoppingControllerTests extends UnitTest {
         Assertions.assertThrows(Exception.class,
             () -> controller.getPrice(new Body(new Item[] {new Item("JACKET", 3)}, STANDARD_CUSTOMER))
         );
+    }
+
+    @ParameterizedTest
+    @CsvSource({"STANDARD_CUSTOMER, 100.0", "PREMIUM_CUSTOMER, 90.0", "PLATINUM_CUSTOMER, 50.0"})
+    void should_return_dicounted_price(ClientType clientType, String dicountedPrice) {
+        Assertions.assertEquals(dicountedPrice, controller.getPrice(new Body(new Item[] {new Item("JACKET", 1)}, clientType)));
     }
 }
